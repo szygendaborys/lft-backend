0a4bfc72f5a09bddec5faa22a9b8e670a30d33b8
Borys Szygenda <44214491+szygendaborys@users.noreply.github.com>
2022-08-13 15:21:15 +0200

    fix: old password must provided in order to change user password (#54)

--- src/users/dto/update.user.dto.ts
+++ src/users/dto/update.user.dto.ts
@@ -1,6 +1,12 @@
-import { PartialType, PickType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType, PickType } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateUserDto } from './create.user.dto';
 
 export class UpdateUserDto extends PartialType(
   PickType(CreateUserDto, ['password']),
-) {}
+) {
+  @ApiPropertyOptional()
+  @IsString()
+  @IsOptional()
+  oldPassword?: string;
+}
